4f0caeb5ab1cad03d66bb39cf0c30545e676d8a7
Eric Ye <ericye16@gmail.com>
2013-10-26 19:18:48 -0400

    Panel now displays, with grid. No colour yet

--- I See Chess/src/ChessPanel.java
+++ I See Chess/src/ChessPanel.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  * Created with IntelliJ IDEA.
@@ -7,25 +9,60 @@ import java.awt.*;
  * Date: 26/10/13
  * Time: 5:32 PM
  */
-public class ChessPanel extends JPanel {
+public class ChessPanel extends JPanel implements MouseListener {
 
     private Board board;
 
     public ChessPanel(Board board) {
+        setOpaque(true);
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
         this.board = board;
+        addMouseListener(this);
     }
 
+    @Override
     public Dimension getPreferredSize() {
         return new Dimension(640,640);
     }
 
+    @Override
+    public Color getBackground() {
+        return Color.WHITE;
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        //do stuff here
+        drawBackground(g);
+    }
+
+    private void drawBackground(Graphics g) {
+        for (int line = 0; line < 8; line++) {
+            int differenceX = getWidth() / 8;
+            int differenceY = getHeight() / 8;
+            //draw the vertical line
+            g.drawLine(line * differenceX, 0, line * differenceX, getHeight());
+            g.drawLine(0, line * differenceY, getWidth(), line * differenceY);
+        }
+
     }
 
     public Board getBoard() {
         return board;
     }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {}
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {}
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {} // we really don't care
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {} //don't care
 }
