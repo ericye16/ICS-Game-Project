39bb36a57bdaa29212e367c871ee117aa96c9e4a
AndreyBorisKhesin <andrey.khesin@gmail.com>
2013-10-26 22:55:28 -0400

    Minor changes.

--- I See Chess/src/ChessPanel.java
+++ I See Chess/src/ChessPanel.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
 /**
  * Created with IntelliJ IDEA.
  * User: eric
@@ -10,31 +9,25 @@ import java.awt.event.MouseListener;
  * Time: 5:32 PM
  */
 public class ChessPanel extends JPanel implements MouseListener {
-
     private Board board;
-
     public ChessPanel(Board board) {
         setOpaque(true);
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
         this.board = board;
         addMouseListener(this);
     }
-
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(640,640);
     }
-
     @Override
     public Color getBackground() {
         return Color.WHITE;
     }
-
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         drawBackground(g);
     }
-
     private void drawBackground(Graphics g) {
         for (int line = 0; line < 8; line++) {
             int differenceX = getWidth() / 8;
@@ -45,24 +38,17 @@ public class ChessPanel extends JPanel implements MouseListener {
         }
 
     }
-
     public Board getBoard() {
         return board;
     }
-
     @Override
-    public void mouseClicked(MouseEvent mouseEvent) {
-    }
-
+    public void mouseClicked(MouseEvent mouseEvent) {}
     @Override
     public void mousePressed(MouseEvent mouseEvent) {}
-
     @Override
     public void mouseReleased(MouseEvent mouseEvent) {}
-
     @Override
     public void mouseEntered(MouseEvent mouseEvent) {} // we really don't care
-
     @Override
     public void mouseExited(MouseEvent mouseEvent) {} //don't care
 }
