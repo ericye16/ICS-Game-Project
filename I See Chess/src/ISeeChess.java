d074d364f8afaf64f33bed5ba44c56c6cd566d50
Eric Ye <ericye16@gmail.com>
2013-10-30 05:32:49 -0400

    Did gui stuff + images

--- I See Chess/src/ISeeChess.java
+++ I See Chess/src/ISeeChess.java
@@ -1,5 +1,20 @@
+import javax.swing.*;
+
 public class ISeeChess {
     public static void main(String[] args){
-        Board board = new Board();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("I See Chess");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new ChessPanel(new Board()));
+        frame.pack();
+        frame.setVisible(true);
     }
 }
