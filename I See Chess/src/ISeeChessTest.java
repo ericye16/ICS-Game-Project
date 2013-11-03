a8c53eae29b73e25855a7bf8d35737ae4bfd5597
Eric Ye <ericye16@gmail.com>
2013-11-03 15:43:18 -0500

    Test case should work now

--- I See Chess/src/ISeeChessTest.java
+++ I See Chess/src/ISeeChessTest.java
@@ -1,3 +1,11 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created with IntelliJ IDEA.
  * User: eric
@@ -5,9 +13,24 @@
  * Time: 1:22 PM
  */
 public class ISeeChessTest {
-    public static void main(String[] args) {
-        Board board = new Board();
-        board.allValidMoves(Piece.WhitePawn, new int[] {3, 1});//breaks
-        board.allValidMoves(Piece.WhitePawn, new int[] {2, 0});//also  breaks
+    Board board = new Board();
+
+    @Test
+    public void testPawn() {
+        ArrayList<Integer[]> pawnPossibles = board.allValidMoves(Piece.WhitePawn, new int[] {3, 1});
+        ArrayList<Integer[]> correct = new ArrayList<Integer[]>();
+        correct.add(new Integer[] {3,2});
+        correct.add(new Integer[] {3,3});
+        System.err.println(correct.size());
+        System.err.println(pawnPossibles.size());
+        assertTrue(correct.size() == pawnPossibles.size());
+        for (Integer[] pawnLoc : pawnPossibles) {
+            for (Integer[] correctLoc: correct) {
+                if (Arrays.equals(pawnLoc, correctLoc)) {
+                    correct.remove(correctLoc);
+                }
+            }
+        }
+        assertTrue(correct.size() == 0);
     }
 }
